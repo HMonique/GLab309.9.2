1641393550306f508ee2b8aa69e72faffc071d37
HMonique <moniquehl@outlook.com>
2024-09-13 14:00:40 -0400

    first commit

---
+++ src/main/java/com/perscholas/UploadPDF/PDF_File/model/FileUploadModel.java
@@ -0,0 +1,14 @@
+package com.perscholas.UploadPDF.PDF_File.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadModel {
+    private String uploadedFile;
+}
+
+
+
---
+++ src/test/java/com/perscholas/UploadPDF/PDF_File/PdfFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.UploadPDF.PDF_File;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
